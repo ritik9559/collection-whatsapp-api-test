15ad66cdce7e14982c6d83727b4d426cc3fe8662
ritik9559 <ritik@surfinmetabharat.com>
2026-06-29 10:52:14 +0530

    debug #3

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ const verifyToken = process.env.VERIFY_TOKEN;
 // Route for GET requests
 app.get('/', (req, res) => {
   const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
-
+  console.log("MODE =", mode);
+  console.log("CHALLENGE =", challenge);
+  console.log("TOKEN =", token);
   if (mode === 'subscribe' && token === verifyToken) {
     console.log('WEBHOOK VERIFIED');
     res.status(200).send(challenge);
